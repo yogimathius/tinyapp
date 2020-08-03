5a7fca70c32265c643fe3f458f47d13a897ab97d
Mathius Johnson <devachievem@gmail.com>
2020-08-03 14:55:55 -0700

    string generator added

--- expressServer.js
+++ expressServer.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
+};
+const generateRandomString = () => {
+  let randomStr = "";
+  const characters = "ABCDEFGHIJKLMNOPQRSTOUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
+  for (let i = 0; i < 6; i++) {
+    randomStr += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return randomStr;
 };
 
 app.get("/", (req, res) => {
@@ -16,6 +26,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
