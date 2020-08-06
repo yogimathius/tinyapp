5c063864d531196e1def28afe06ea0ac6b3df603
Mathius Johnson <devachievem@gmail.com>
2020-08-06 23:58:32 +0000

    refactored, passwords store, ready for submission

---
+++ test/helpersTest.js
@@ -0,0 +1,36 @@
+const { assert } = require('chai');
+
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
+
+describe('getUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedOutput = "userRandomID";
+    // Write your assert statement here
+    assert.equal(user, expectedOutput);
+  });
+  it('should return undefined if email field is left blank', function() {
+    const user = getUserByEmail("", testUsers);
+    const expectedOutput = undefined;
+		
+    assert.equal(user, expectedOutput);
+  });
+});
+
+
+// (Stretch) the date the short URL was created
+// (Stretch) the number of times the short URL was visited
+// (Stretch) the number number of unique visits for the short URL
